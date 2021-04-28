5885cbe2dfe637f87fee54762c0af1e92408a0f0
tejaswini <tejaswinichandruhc@gmail.com>
2021-04-28 10:39:58 -1200

    rebase commit

--- src/main/scala/feature_branch1_code.scala
+++ src/main/scala/feature_branch1_code.scala
@@ -2,7 +2,7 @@ object feature_branch1_code {
   def main(args: Array[String]): Unit = {
     print("this is feature branch 2")
     println("commit and rebuse to integtion branch")
-    print("hello")
+    print("hello rabase")
   }
 
 }

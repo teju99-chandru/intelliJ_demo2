4ee1a47c257b8783a143d959abc7c7270fe7cc38
tejaswini <tejaswinichandruhc@gmail.com>
2021-04-28 09:52:54 -1200

    first commit

--- src/main/scala/feature_branch2_code.scala
+++ src/main/scala/feature_branch2_code.scala
@@ -1,5 +1,9 @@
 object feature_branch2_code {
   def main(args: Array[String]): Unit = {
     println("hello")
+<<<<<<< HEAD
+=======
+    println("first commit")
+>>>>>>> 835d2eb (first commit)
   }
 }
